d80b69ea19a984547a6f889ebb3a0351166732e7
franmacario <franpms89@gmail.com>
2025-07-11 10:37:14 -0300

    OrderStatus enum

--- src/main/java/com/product/product/entities/Order.java
+++ src/main/java/com/product/product/entities/Order.java
@@ -1,6 +1,7 @@
 package com.product.product.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.product.product.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -16,9 +17,11 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
-    
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name="client_id")
@@ -26,10 +29,11 @@ public class Order implements Serializable {
 
     public Order() {
     }
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -54,6 +58,17 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus == null) {
+            throw new IllegalArgumentException("Order status cannot be null");
+        }
+        this.orderStatus = orderStatus.getCode();
     }
 
     @Override
